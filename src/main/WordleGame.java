fa6c4519a7009703469b08215f7e1086d7790bea
Graham Arnett <GrahamKentArnett@outlook.com>
2025-02-15 14:27:21 -0500

    init commit

---
+++ src/main/WordleGame.java
@@ -0,0 +1,24 @@
+package main;
+import java.util.Scanner;
+
+public class WordleGame {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Welcome to my Wordle App\n"
+				+ "The rules:"
+				+ "\nYou have 6 guesses"
+				+ "\nThe word is a valid 5 letter word"
+				+ "\nA green letter is correct and in the right spot"
+				+ "\nA yellow letter is correct but in the wrong place"
+				+ "\nWhite is not used at all");
+		
+		int turns = 0;
+		
+		boolean victory = false;//ok, in java its boolean not bool, noted
+		
+		
+	}
+
+}
